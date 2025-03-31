5c6a567865cfe2f0d6d8d94ba8f0a47ddd2501d5
Janarthan-GM <janarthangm7@gmail.com>
2025-03-31 12:27:12 +0530

    init commit

--- src/main/java/com/example/jobportal/config/SwaggerConfig.java
+++ src/main/java/com/example/jobportal/config/SwaggerConfig.java
@@ -1,19 +1,34 @@
 package com.example.jobportal.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Job Portal API")
-                        .version("1.0")
-                        .description("API documentation for Job Portal Application"));
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.jobportal.Controller")) // Adjust package name if needed
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfoBuilder()
+                .title("Job Portal API")
+                .description("API documentation for Job Portal Application")
+                .version("1.0")
+                .build();
     }
 }
